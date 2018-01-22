9b577c4d7a24cb1fa6fae514b487a386ed4f4dee
wessel-henkes <wessel.henkes@casema.nl>
2018-01-22 15:27:54 +0100

    add jdbc to maven

---
+++ src/main/java/connection/testConnect.java
@@ -0,0 +1,45 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class testConnect {
+    private ConnectionOracle CO;
+    private Connection conn  = CO.getConn();
+    private Connection conn2 = CO.getConn2();
+
+    public testconneasdf(){
+        //test
+        Statement stm = conn.createStatement();
+        String queryText = "SELECT id, string FROM test";
+        Statement stm2 = conn2.createStatement();
+        String queryText2 = "SELECT id , string FROM test";
+
+        ResultSet rs = stm.executeQuery(queryText);
+        ResultSet rs2 = stm2.executeQuery(queryText2);
+
+        int id;
+        String string;
+        while (rs.next()) {
+            id = rs.getInt("id");
+            string = rs.getString("string");
+            System.out.println(id + ", " + string);
+        }
+
+        int id2;
+        String string2;
+        while (rs2.next()) {
+            id2 = rs2.getInt("id");
+            string2 = rs2.getString("string");
+            System.out.println(id2 + ", " + string2);
+        }
+
+        rs.close();
+        stm.close();
+        rs2.close();
+        stm2.close();
+        conn.close();
+        conn2.close();
+    }
+}
