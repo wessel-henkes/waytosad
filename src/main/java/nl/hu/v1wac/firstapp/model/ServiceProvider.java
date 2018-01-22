246db6fc8b84bfe2d0ad3a20ecf19446d3d99df6
wessel-henkes <wessel.henkes@casema.nl>
2018-01-22 13:29:34 +0100

    test hello world rest service

--- src/main/java/nl/hu/v1wac/firstapp/model/ServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.v1wac.firstapp.model;
-
-public class ServiceProvider {
-	private static CountryService worldService = new CountryService();
-
-	public static CountryService getWorldService() {
-		return worldService;
-	}
-}
